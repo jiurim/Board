5cecf2245c30e983ffd9d0db996049f73550bf19
urim <jiurim0316@naver.com>
2023-07-17 23:16:20 +0900

    게시판 리스트 작성

--- src/main/java/com/study/board/Controller/BoardController.java
+++ src/main/java/com/study/board/Controller/BoardController.java
@@ -1,6 +1,9 @@
 package com.study.board.Controller;
 
+import org.springframework.ui.Model;
 import com.study.board.entity.Board;
+import com.study.board.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +12,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BoardController {
+    @Autowired
+    private BoardService boardService;
 
     @GetMapping("/board/write") //localhost:8080/board/write 이 주소에 접속하면 boardwrite html을 보여줌
     public String boardWriteForm(){
@@ -18,9 +23,17 @@ public class BoardController {
 
     @PostMapping("/board/writepro")
     public String boardWritepro(Board board){
-        System.out.println(board.getTitle());
+        boardService.write(board);
         return "";
     }
 
+    @GetMapping("/board/list")
+    public String boardList(Model model){
+        model.addAttribute("list",boardService.boardList());
+
+
+
+        return "boardlist";
+    }
 
 }
